66a3a9973da7c4cdb84b2a65f4595aaf6535c396
deekshithareddyaknapuram <deekshithareddyaknapuram@gmail.com>
2026-03-04 20:09:39 +0530

    Create insertion at end in ll.py

---
+++ insertion at end in ll.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+n=int(input())
+head=None
+temp=None
+for i in range(n):
+    val=int(input())
+    new_node=Node(val)
+    if head is None:
+        head=new_node
+        temp=head
+    else:
+        temp.next=new_node
+        temp=temp.next
+ele=int(input())
+new_node=Node(ele)
+temp=head
+while temp.next:
+    temp=temp.next
+
+temp.next=new_node
+
+
+
+temp=head
+while temp:
+    print(temp.data,end=" ->")
+    temp=temp.next
+print('None')
